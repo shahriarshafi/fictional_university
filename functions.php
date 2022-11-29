7bc39c7076ef5431cb4ad20a06258b544fa5cf73
shahriarshafi <shahriarshafi@gmail.com>
2022-11-29 12:45:52 +0600

    Changed functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 function university_files(){
     wp_enqueue_script('slider-js' , get_theme_file_uri('/src/index.js') , NULL ,'1.0' ,true);
 
-    wp_enqueue_script('main-website-js' , get_theme_file_uri('/build/index.js') , NULL, '1.0' , true);
+    // wp_enqueue_script('main-website-js' , get_theme_file_uri('/build/index.js') , NULL, '1.0' , true);
 
     wp_enqueue_style('google_font' , '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 
